449987eb6e5e60f998964a275bbcd5fed3343b39
Vijay <vijaykumar.968@hotmail.com>
2022-02-18 20:30:21 -0600

    make it better

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // bypass cross origin policiy
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://clever-agnesi-768a96.netlify.app"],
     methods: ["GET", "POST", "PUT"],
     credentials: true
 }));

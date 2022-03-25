aeaa5542a8808c6deec93b961a7c8818bb861214
Vijay <vijaykumar.968@hotmail.com>
2022-03-25 13:36:38 -0600

    Test 2.0

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // bypass cross origin policiy
 app.use(cors({
-    origin: ["https://main.d3r62sci50fysg.amplifyapp.com"],
+    origin: ["https://www.tritonsrm.com"],
     methods: ["GET", "POST", "PUT"],
     credentials: true
 }));

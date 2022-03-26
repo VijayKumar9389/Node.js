3a55a75ced418701a551d285da14dcf476051d25
Vijay <vijaykumar.968@hotmail.com>
2022-03-26 00:13:45 -0600

    api https test

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // bypass cross origin policiy
 app.use(cors({
-    origin: ["https://www.tritonsrm.com"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT"],
     credentials: true
 }));

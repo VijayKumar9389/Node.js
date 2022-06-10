3960d7b7a59ff02811bd745766a69085fa0555e9
Vijay <vijaykumar.968@hotmail.com>
2022-06-10 17:15:38 -0600

    Access Token Update

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -2,23 +2,30 @@ const AuthModel = require("../models/auth.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-function generateAccessToken(user){
-    return jwt.sign({user}, process.env.JWT_SECRET, { expiresIn: '15m' });
+//creates access token
+function generateAccessToken(user) {
+    return jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '5m' });
 }
 
 //Login into account
 exports.Login = (req, res) => {
+
     const tmp = req.body
+
     AuthModel.Login(tmp, (err, user) => {
-        if (err)
-            res.send(err);
+
+        if (err) res.send(err);
+
+        //checks if any results are returned from DB
         if (user.length > 0) {
             //decrypts password from the database 
             bcrypt.compare(tmp.password, user[0].PASSWORD, (err, response) => {
                 if (response) {
+                    //generates tokens
                     const token = generateAccessToken(user);
-                    const refreshToken = jwt.sign({user}, process.env.JWT_SECRET, { expiresIn: '24hr' });
-                    res.send({ auth: true, token: token, refreshToken: refreshToken});
+                    const refreshToken = jwt.sign({ user }, process.env.JWT_REFRESH_SECRET, { expiresIn: '8hr' });
+                    //sends tokens
+                    res.send({ auth: true, token: token, refreshToken: refreshToken });
                     console.log(user[0].USERNAME + " Logged in successfully");
                 } else {
                     res.send({ auth: false, message: "Wrong username or password" });
@@ -35,15 +42,18 @@ exports.getLogin = (req, res) => {
 
     const token = req.headers["x-access-refresh-token"];
 
+    //checks if clients has a valid refresh token
     if (!token) {
-        res.send({ auth: false })
+        res.send({ auth: false });
     } else {
-        jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+        //verifys refresh token
+        jwt.verify(token, process.env.JWT_REFRESH_SECRET, (err, decode) => {
             if (err) {
                 res.send({ auth: false })
             } else {
-                const accessToken = generateAccessToken({username: 'user'})
-                res.send({ auth: true, token: accessToken})
+                //returns a new access token
+                const accessToken = generateAccessToken({ username: 'user' })
+                res.send({ auth: true, token: accessToken })
             }
         });
     }
@@ -51,9 +61,6 @@ exports.getLogin = (req, res) => {
 
 // logs user out
 exports.Logout = (req, res) => {
-    req.session.destroy();
+    const token = req.headers["x-access-refresh-token"];
 }
 
-// exports.Token = (req, res) => {
-//     const refreshToken = req.body.token;
-// }
